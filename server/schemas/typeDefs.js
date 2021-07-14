f8cd8a76966eb1c303dce655feed8bbf9cedeeb0
Noemiegrau <noemiegrau@outlook.fr>
2021-07-14 13:21:48 -0700

    added typeDefs + auth.js + modified server.js to include apollo + resolvers + index.js in schema

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,7 +2,43 @@
 const { gql } = require('apollo-server-express');
 
 // create our typeDefs
-const typeDefs = gql``;
+const typeDefs = gql`
+type Auth {
+    token: ID!
+    user: User
+  }
+  
+type Book {
+    bookId: ID
+    authors: []
+    description: String
+    title: String
+    image: ??????????????
+    link: [Reaction]
+  }
+
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: [Author]!, description: String!, title: String!, bookId: ID!, image: img!, ): User
+    removeBook(bookId: ID!): User
+  }
+
+`;
 
 // export the typeDefs
 module.exports = typeDefs;
